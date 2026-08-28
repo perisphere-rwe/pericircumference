f872afc2dc1f46005e1d9f976cdcd306ad2339e5
TylerSagendorf <tjsagendorf@gmail.com>
2026-08-28 09:56:22 -0500

    Add background fills to flextable_polish

--- inst/templates/flextable.R
+++ inst/templates/flextable.R
@@ -5,7 +5,8 @@ flextable_polish <- function(ft,
                              font_size = 11,
                              header_text = NULL,
                              footer_text = NULL,
-                             bold_row_index = NULL) {
+                             bold_group_labels = TRUE,
+                             use_peripal = TRUE) {
 
   if(!is.null(header_text))
     ft <- ft %>% add_header_lines(header_text)
@@ -13,8 +14,20 @@ flextable_polish <- function(ft,
   if(!is.null(footer_text))
     ft <- ft %>% add_footer_lines(footer_text)
 
-  if(!is.null(bold_row_index))
-    ft <- ft %>% bold(i = bold_row_index, j = 1L)
+  if (use_peripal)
+    ft <- ft %>% bg(i = 1L, bg = "#95B2DE", part = "header")
+
+  is_grouped <- class(ft$body$dataset)[1L] == "grouped_data"
+
+  if (is_grouped) {
+    group_label_idx <- which(!is.na(ft$body$dataset[[1L]]))
+
+    if (bold_group_labels)
+      ft <- ft %>% bold(i = group_label_idx, j = 1L)
+
+    if (use_peripal)
+      ft <- ft %>% bg(i = group_label_idx, bg = "lightblue")
+  }
 
   ft %>%
     bold(part = 'header') %>%
